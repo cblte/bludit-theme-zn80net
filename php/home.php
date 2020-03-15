d57b83be36e06020ff216690eee9e5986346c3d7
cblte <https://scr.im/cblteme>
2020-03-15 17:12:32 +0100

    moved commet counter into seperate file

--- php/home.php
+++ php/home.php
@@ -1,28 +1,8 @@
 <?php foreach ($content as $page) : ?>
 <!-- A single blog post on the homepage -->
 <article <?php if ($page->custom('link')) {?>class="link" <?php } ?>>
-<?php 
-// CHECK IF PLUGIN IS LOADED
-$pageUuid = $page->getValue('uuid');
-$commentCounter = 0;
-
-foreach($SnickerIndex->db as $id => $comment) {
-	if($comment['page_uuid'] == $pageUuid && $comment['status'] == 'approved') $commentCounter++;
-}
-
-if ($commentCounter == 0) {
-	$commentText = $L->get('no-comments');
-} elseif ($commentCounter = 1) {
-	$commentText = $L->get('one-comment');
-} else {
-	$commentText = sprintf($L->get('comments'), $commentCounter );
-}
-?>
 <!-- Load Bludit Plugins: Page Begin -->
 <?php Theme::plugins('pageBegin'); ?>
-<?php if ($page->coverImage()) : ?>
-	<img class="coverImage" alt="Cover Image for <?php echo $page->title(); ?>" src="<?php echo $page->coverImage(); ?>" />
-<?php endif ?>
 	<h2>
 		<?php if ($page->custom('link')) { ?>
 		<a href="<?php echo $page->custom('link'); ?>"><?php echo $page->title(); ?></a> 
@@ -36,13 +16,13 @@ if ($commentCounter == 0) {
 		&ndash; 
 		<?php echo $L->get('Reading time') . ': ' . $page->readingTime(); ?>
 		&ndash; 
-		<a href="<?php echo $page->permalink(); ?>#comments"><?php echo $commentText ?></a>
+		<a href="<?php echo $page->permalink(); ?>#comments"><?php include(THEME_DIR_PHP . 'comments-counter.php') ?></a>
 	</small>
 	<!-- Breaked content -->
 	<?php echo $page->contentBreak(); ?>
-	<?php if ($page->readMore()) : // Read more" button ?>
+	<?php if ($page->readMore()) { // Read more" button ?>
 	<a href="<?php echo $page->permalink(); ?>" title="Permalink: <?php echo $page->title(); ?>"><?php echo $L->get('Read more'); ?></a>
-	<?php endif ?>
+	<?php } ?>
 	<?php Theme::plugins('pageEnd'); // Load Bludit Plugins: Page End ?>
 </article>
 <?php endforeach; ?>
